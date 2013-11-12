03e78cab821e73252298ea3070c2283e2f0f57c7
Fernando <fermarichal@hotmail.com>
2013-11-12 20:02:29 -0200

    agregado el jquery

--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,5 @@
 <?php
 
     echo '<script type="text/javascript" src="'.APPPATH.'static/js/example.js"></script>';
+    echo '<script type="text/javascript" src="'.APPPATH.'static/js/jquery-2.0.2.min.js"></script>';
 echo "Aquí se cargan los scritps";
