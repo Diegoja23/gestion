4416c3a46b70bb0826355986f89278767009ba28
Fernando <fermarichal@hotmail.com>
2013-11-17 21:55:45 -0200

    ver todos clientes

--- application/static/js/script_gestion.js
+++ application/static/js/script_gestion.js
@@ -1,29 +1,25 @@
 $(document).ready(iniEventos);
 
 function iniEventos() {
-     /*$('#btn_acceso').on('click',validarRegistro);
-     $('#btn_crear').on('click',crearCopia);
-     $('#btn_agregar_sinonimo').on('click',agregarSinonimo);  
-     $('#btn_mostrar_agregar_sinonimo').on('click',mostrarAgregarSinonimo); */
-    //alert("asdfasdf");
-     
-     //cargar();
+    $("#div_listado_cliente").load("http://localhost/gestion/consultas/consultas_clientes.php",{consulta: "traer_todos"}); 
 }
 
 $(document).on("click","#agregar_cliente",agregarCliente);
+$(document).on("click","#guardar_cliente",guardarCliente);
 
 function agregarCliente(){
-    if($("#div_formulario_cliente").css("display") == "none"){
-        $("#div_formulario_cliente").fadeIn(1500);
-        //$("#div_formulario_cliente").css("display","block");
+    if($("#div_formulario_cliente").css("display") == "none"){ 
         $("#div_listado_cliente").fadeOut(1500);
-        //$("#div_listado_cliente").css("display","none");        
+        $("#div_formulario_cliente").fadeIn(1500);
+   
     }
     else{
         $("#div_formulario_cliente").fadeOut(1500);
-        //$("#div_formulario_cliente").css("display","none");
         $("#div_listado_cliente").fadeIn(1500);
-        //$("#div_listado_cliente").css("display","block"); 
     }
+}
+
+function guardarCliente(){
+    $("#div_formulario_cliente").load("http://localhost/gestion/consultas/consultas_clientes.php",{consulta: "agregar_cliente"});
 }
 //$( document ).on( "change",".elegir_familia", cambiarElemento );
--- application/views/consultas/consultas_clientes.php
+++ application/views/consultas/consultas_clientes.php
@@ -1,7 +1,72 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$consulta = $_POST['consulta'];
+
+switch($consulta){
+    case "traer_todos":
+        echo traerTodos();
+        //echo "traigo todos";
+        break;
+
+    case "agregar_cliente": 
+        $un_cliente = cargarValores();
+        if($Fachada->agregarCliente($paramsCliente)){
+            echo "El cliente ". $paramsCliente['nombre']." se ingresó con éxito<br>";
+        }
+        else{
+            echo "El cliente ". $paramsCliente['nombre']." no pudo ser ingresado. Verifique los datos<br>";   
+        }
+        break;
+    
+    case 2:
+        echo "a es igual a 2";
+        break;
+    default:
+        echo "a no es igual a ninguno de los valores esperados";
+        break;
+}
+
+function cargarValores(){   
+    $paramsCliente=array();
+    $paramsCliente['nombre']=$_POST['nombre'];
+    $paramsCliente['apellido']=$_POST['apellido'];
+    $paramsCliente['ci']=$_POST['ci'];
+    $paramsCliente['email']=$_POST['email'];
+    $paramsCliente['telefono']=$_POST['telefono'];
+    $paramsCliente['direccion']=$_POST['direccion']; 
+    return $paramsCliente;
+}
+function traerTodos(){
+    $todos_los_clientes = '<table class="table table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>';
+    return $todos_los_clientes;
+}
+
 ?>
