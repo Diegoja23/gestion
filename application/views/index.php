ea9b8234a63312937015601a5b40102d6c9f5c9a
Fernando <fermarichal@hotmail.com>
2014-01-27 20:49:46 -0200

    a

--- application/views/index.php
+++ application/views/index.php
@@ -48,6 +48,28 @@
                 Rol: <em><?php if($el_usuario['rol']==1){echo 'Administrador';} else{if($el_usuario['rol']==2){echo 'Editor';}};?></em>
             </p>
             <a href="logout" class="btn btn-warning btn-xs float-right">Salir</a>
+            
+            <ul class="nav nav-tabs" id="barraFiltros">                            
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="">
+                        Curso<span class="caret"></span>
+                    </a>
+                <ul id='menuCurso' class="dropdown-menu">
+
+                </ul>
+                </li>                         
+                <li class="dropdown">
+                    <a class="dropdown-toggle" style='margin-right: 10px;' data-toggle="dropdown" href="">
+                        Año<span class="caret"></span>
+                    </a>
+                    <ul id='menuFecha' class="dropdown-menu">
+
+                    </ul>
+                </li>
+                    <input type="text" class="form-control casilla" id="txtEmail" placeholder="E-mail" size="30" value=""/> 
+                    <button type="button" id='btnBuscar' class="btn btn-primary">Buscar</button>
+            </ul>
+            
         </div>
         <img class="paperclip" src="<?=APPPATH?>static/images/paperclip.png" alt="paperclip" />
         <div class="sidebar">
